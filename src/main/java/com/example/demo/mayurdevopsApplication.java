603013cd5693996bf245857b2042f545941924af
Mayur Magar <96387286+mayur702@users.noreply.github.com>
2024-09-06 14:07:52 +0530

    Update mayurdevopsApplication.java

--- src/main/java/com/example/demo/mayurdevopsApplication.java
+++ src/main/java/com/example/demo/mayurdevopsApplication.java
@@ -1,12 +1,12 @@
-package com.example.sendevops;
+package com.example.mayurdevops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SendevopsApplication {
+public class mayurdevopsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SendevopsApplication.class, args);
+        SpringApplication.run(mayurdevopsApplication.class, args);
     }
 }
