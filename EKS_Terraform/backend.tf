4d762396fcc8bf16fa6b291f9661cad4388409e5
Mayur Magar <96387286+mayur702@users.noreply.github.com>
2024-12-01 23:04:15 +0530

    Update backend.tf

--- EKS_Terraform/backend.tf
+++ EKS_Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mayur07" # Replace with your actual S3 bucket name
+    bucket = "mayur007" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
